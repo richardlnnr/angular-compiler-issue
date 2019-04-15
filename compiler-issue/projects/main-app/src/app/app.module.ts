d9c804ef71c197a6caf61a343515e9d9ab0ecc09
Richard Lindner <richard.lnnr@gmail.com>
2019-04-15 17:37:11 -0300

    Add home component Add bootstrap

--- compiler-issue/projects/main-app/src/app/app.module.ts
+++ compiler-issue/projects/main-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NormalStrategyComponent } from './normal-strategy/normal-strategy.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NormalStrategyComponent
+    NormalStrategyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
